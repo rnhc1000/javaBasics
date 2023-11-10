4a60f43ada2a01d42ad5c22cbac04e1dd258bdcd
Ricardo Ferreira <rnhc1000@hotmail.com>
2023-11-10 20:30:01 -0300

    Encapsulation, Constructors and Overriding

---
+++ chess/src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import boardgame.Position;
+
+/**
+ * Encapsulation Constructors ToString(overriding)
+ */
+public class Program {
+
+	public static void main(String[] args) {
+		Position position = new Position(1, 3);
+		System.out.println(position);
+	}
+
+}
